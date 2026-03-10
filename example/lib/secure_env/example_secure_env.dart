85862583d1629ec0cd96317f574cfc458b1e7eba
zyyziyunying <zyyziyunying@163.com>
2026-03-10 21:00:58 +0800

    功能：更新 .gitignore 文件以排除示例项目的配置，添加 Flutter 项目的元数据文件及示例环境文件

--- example/lib/secure_env/example_secure_env.dart
+++ example/lib/secure_env/example_secure_env.dart
@@ -7,8 +7,8 @@ part "example_secure_env.g.dart";
   fieldRename: FieldRename.screamingSnake,
 )
 abstract class ExampleSecureEnv {
-  static const _encryptionKey = "YOUR_BASE64_ENCRYPTION_KEY";
-  static const _iv = "YOUR_BASE64_IV";
+  static const _encryptionKey = "3D0xOo+/qJN6CiAWTXIXCryC1vKB8TSvkO45aiN2HFA=";
+  static const _iv = "1kPHE8gtNT+SEGU/MOtu1Q==";
   static ExampleSecureEnv create() {
     return ExampleSecureEnv(_encryptionKey, _iv);
   }
